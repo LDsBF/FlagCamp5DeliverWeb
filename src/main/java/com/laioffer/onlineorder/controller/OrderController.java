ade37b61849c6de8f40e3511dd9c808f420fb747
LDsBF <tw2774@columbia.edu>
2022-09-16 19:20:51 -0400

    First structure draft of project

--- src/main/java/com/laioffer/onlineorder/controller/OrderController.java
+++ src/main/java/com/laioffer/onlineorder/controller/OrderController.java
@@ -4,6 +4,9 @@ import com.laioffer.onlineorder.entity.Order;
 import com.laioffer.onlineorder.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -12,6 +15,8 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @RequestMapping(value = "/orders", method = RequestMethod.GET)
+    @ResponseBody
     public List<Order> trackOrders() {
         // TODO get current login user
         return orderService.getOrders("");
