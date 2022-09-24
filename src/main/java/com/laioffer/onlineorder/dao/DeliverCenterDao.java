c37681c5f4ef491f318399586c83b598bc1fa430
iiisso <zhenduowang@umass.edu>
2022-09-23 22:24:12 -0400

    DelieverCenterDao implemented

--- src/main/java/com/laioffer/onlineorder/dao/DeliverCenterDao.java
+++ src/main/java/com/laioffer/onlineorder/dao/DeliverCenterDao.java
@@ -3,9 +3,13 @@ package com.laioffer.onlineorder.dao;
 import com.laioffer.onlineorder.entity.DeliverCenter;
 import com.laioffer.onlineorder.entity.DeliverInfo;
 import org.hibernate.SessionFactory;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -15,6 +19,15 @@ public class DeliverCenterDao {
 
     public List<DeliverCenter> getAllValidCenters(DeliverInfo deliverInfo) {
         // TODO
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<DeliverCenter> criteria = builder.createQuery(DeliverCenter.class);
+            criteria.from(DeliverCenter.class);
+            return session.createQuery(criteria).getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return new ArrayList<>();
     }
 }
